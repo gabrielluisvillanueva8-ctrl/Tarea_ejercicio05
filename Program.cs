a72f6d1f737efe15c1bce3defc31691f7cbef025
gabrielluisvillanueva8-ctrl <gabrielluisvillanueva8@gmail.com>
2026-09-14 17:55:45 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+class Program
+{
+    static int ContarIguales(int[] notas, int valor)
+    {
+        int contador = 0;
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if (notas[i] == valor)
+            {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    static int ContarAprobados(int[] notas)
+    {
+        int contador = 0;
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if (notas[i] >= 13)
+            {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    static int ContarDesaprobados(int[] notas)
+    {
+        int contador = 0;
+        for (int i = 0; i < notas.Length; i++)
+        {
+            if (notas[i] < 13)
+            {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    static void Main()
+    {
+        int N;
+        do
+        {
+            Console.Write("Ingrese la cantidad de alumnos (máximo 10): ");
+            N = int.Parse(Console.ReadLine());
+        }
+        while (N <= 0 || N > 10);
+        int[] notas = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write("Nota " + (i + 1) + ": ");
+            notas[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("\nCantidad de personas que obtuvieron 00: " + ContarIguales(notas, 0));
+        Console.WriteLine("Cantidad de personas que obtuvieron 20: " + ContarIguales(notas, 20));
+        Console.WriteLine("Cantidad de personas aprobadas: " + ContarAprobados(notas));
+        Console.WriteLine("Cantidad de personas desaprobadas: " + ContarDesaprobados(notas));
+        Console.ReadKey();
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Tarea_ejercicio05")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tarea_ejercicio05")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e5aed1c1-79e8-47f6-81b8-7b6dd20d214c")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
